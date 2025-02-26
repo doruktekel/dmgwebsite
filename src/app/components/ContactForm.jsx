65f93da7fabdf02db5a9a57c856d994956fc702b
doruk.tekel <doruktekel55@gmail.com>
2025-02-26 21:09:38 +0300

    recaptcha deleted

--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -1,117 +1,3 @@
-// "use client";
-
-// import { useState } from "react";
-// import emailjs from "emailjs-com";
-// import { toast } from "react-toastify";
-
-// export default function ContactForm() {
-//   const [formData, setFormData] = useState({
-//     name: "",
-//     phone: "",
-//     email: "",
-//     message: "",
-//   });
-
-//   const [loading, setLoading] = useState(false);
-//   const [success, setSuccess] = useState(null);
-
-//   const handleChange = (e) => {
-//     setFormData({ ...formData, [e.target.name]: e.target.value });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     setLoading(true);
-
-//     try {
-//       const res = await emailjs.send(
-//         process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, // process.env.EMAILJS_SERVICE_ID,  EmailJS Service ID
-//         process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
-//         formData, // process.env.EMAILJS_TEMPLATE_ID, EmailJS Template ID
-//         process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_ID //process.env.EMAILJS_PUBLIC_ID // EmailJS Public Key
-//       );
-
-//       if (res.status === 200) {
-//         setSuccess("Mesaj başarıyla gönderildi!");
-//         toast.success("Mesaj başarıyla gönderildi!", {
-//           position: "top-right",
-//           autoClose: 5000,
-//           hideProgressBar: false,
-//           closeOnClick: false,
-//           pauseOnHover: true,
-//           draggable: true,
-//           progress: undefined,
-//           theme: "light",
-//         });
-//         setFormData({ name: "", phone: "", email: "", message: "" });
-//       }
-//     } catch (error) {
-//       setSuccess("Mesaj gönderilirken hata oluştu.");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   return (
-//     <form
-//       onSubmit={handleSubmit}
-//       className="w-full mx-auto flex flex-col gap-2 "
-//     >
-//       <p className=" text-center font-Poppins text-lg">Bize Ulaşın</p>
-//       <p className="font-Poppins font-extralight">
-//         İsim <span className="text-red-600">*</span>
-//       </p>
-//       <input
-//         type="text"
-//         name="name"
-//         value={formData.name}
-//         onChange={handleChange}
-//         className="w-full px-2 py-1 border border-gray-300  text-sm "
-//         required
-//       />
-//       <p className="font-Poppins font-extralight">
-//         Telefon <span className="text-red-600">*</span>
-//       </p>
-//       <input
-//         type="tel"
-//         name="phone"
-//         value={formData.phone}
-//         onChange={handleChange}
-//         className="w-full px-2 py-1 border border-gray-300  text-sm "
-//         required
-//       />
-//       <p className="font-Poppins font-extralight">
-//         Email <span className="text-red-600">*</span>
-//       </p>
-//       <input
-//         type="email"
-//         name="email"
-//         value={formData.email}
-//         onChange={handleChange}
-//         className="w-full px-2 py-1 border border-gray-300  text-sm "
-//         required
-//       />
-//       <p className="font-Poppins font-extralight">
-//         Mesaj <span className="text-red-600">*</span>
-//       </p>
-//       <textarea
-//         name="message"
-//         value={formData.message}
-//         onChange={handleChange}
-//         className="w-full px-2 py-1 border border-gray-300  text-sm "
-//         required
-//       />
-//       <button
-//         type="submit"
-//         className=" max-w-40 bg-gray-800 text-white p-2 rounded disabled:bg-gray-400  font-Poppins "
-//         disabled={loading}
-//       >
-//         {loading ? "Gönderiliyor..." : "Gönder"}
-//       </button>
-//     </form>
-//   );
-// }
-
 "use client";
 
 import { useState } from "react";
@@ -137,23 +23,12 @@ export default function ContactForm() {
     e.preventDefault();
     setLoading(true);
 
-    // Google reCAPTCHA V3 Token Al
     try {
-      const token = await new Promise((resolve, reject) => {
-        if (typeof window !== "undefined") {
-          window.grecaptcha
-            .execute(process.env.NEXT_SITE_KEY, { action: "submit" })
-            .then((token) => resolve(token))
-            .catch((err) => reject("reCAPTCHA token alınamadı"));
-        }
-      });
-
-      // Form verileriyle birlikte reCAPTCHA token'ı gönder
       const res = await emailjs.send(
-        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, // EmailJS Service ID
-        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID, // EmailJS Template ID
-        { ...formData, recaptchaToken: token }, // reCAPTCHA token'ı dahil et
-        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_ID // EmailJS Public Key
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID, // process.env.EMAILJS_SERVICE_ID,  EmailJS Service ID
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        formData, // process.env.EMAILJS_TEMPLATE_ID, EmailJS Template ID
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_ID //process.env.EMAILJS_PUBLIC_ID // EmailJS Public Key
       );
 
       if (res.status === 200) {
